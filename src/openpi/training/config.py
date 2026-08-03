5231c71bf862e21fd6e4918b391eb572cbd09c76
CarlDegio <2536890272@qq.com>
2026-08-03 16:59:28 +0800

    change mix setting

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -1012,7 +1012,7 @@ _CONFIGS = [
     _make_g1_sonic_full_config("pi05_g1_sonic_full_movedoor_4cam", "MoveDoorMerge4Cam"),
     _make_g1_sonic_full_config("pi05_g1_sonic_full_clean_desk", "CleanDeskMerge"),
     _make_g1_sonic_full_config("pi05_g1_sonic_full_walk_clean_desk", "WalkCleanDesk"),
-    _make_g1_sonic_full_config("pi05_g1_sonic_full_walk_clean_desk_mix", "WalkCleanDesk_mix_CleanDesk"),
+    _make_g1_sonic_full_config("pi05_g1_sonic_full_walk_clean_desk_mix", "WalkCleanDesk_mix"),
     
     #
     # Fine-tuning Aloha configs.
